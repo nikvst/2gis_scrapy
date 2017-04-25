87dd5a3af7c608bb3c2b0925181d9fd2f2604778
Nikita Vstovsky <nikvst@gmail.com>
2017-04-26 02:39:12 +1000

    Реализация парсинга данных с 2gis

--- gis_scrapy/settings.py
+++ gis_scrapy/settings.py
@@ -21,7 +21,7 @@ NEWSPIDER_MODULE = 'gis_scrapy.spiders'
 USER_AGENT = 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
